f943c1a9a99fb79833185866383b5b8090678fcd
arsam <steamarsam70@gmail.com>
2023-12-02 17:04:42 +0330

    add keyboard clicking function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import { useEffect, useState } from "react"
 import "./style.css"
-
 export function App() {
     const [show , setShow] = useState("")
     const [showResult , setShowResult] = useState("")
     const [error , setError] = useState(false)
     // const [typing , setTyping] = useState("")
+    
+    window.addEventListener("keydown" , (e) => {
+        
+        const button = document.querySelector("#click")
+        const b9 = document.querySelector(".b9")
+        const b8 = document.querySelector(".b8")
+        const b7 = document.querySelector(".b7")
+        const b6 = document.querySelector(".b6")
+        const b5 = document.querySelector(".b5")
+        const b4 = document.querySelector(".b4")
+        const b3 = document.querySelector(".b3")
+        const b2 = document.querySelector(".b2")
+        const b1 = document.querySelector(".b1")
+        const b0 = document.querySelector(".b0")
 
+        let a = e.key
+        if(a == "Enter") {
+            
+            button.click()
+        }else if(a == "9") {
+            
+            b9.click()
+        }else if(a == "8") {
+            b8.click()
+        }else if(a == "7") {
+            b7.click()
+        }else if(a == "6") {
+            b6.click()
+        }else if(a == "5") {
+            b5.click()
+        }else if(a == "4") {
+            b4.click()
+        }else if(a == "3") {
+            b3.click()
+        }else if(a == "2") {
+            b2.click()
+        }else if(a == "1") {
+            b1.click()
+        }else if(a == "0") {
+            b0.click()
+        }
+    } , false)
     return(
         <>
 
@@ -68,38 +108,38 @@ export function App() {
                     flexWrap:"wrap",
                     gap:"10px"
                 }}>
-                    <button onClick={() => {
+                    <button className="b9" onClick={() => {
                             
                                     
                                     setShow(show +9)
 
                         
                     }}>9</button>
-                    <button onClick={() => {
+                    <button className="b8" onClick={() => {
                         
                         
                             setShow(show +8)
                         
                     }}>8</button>
-                    <button onClick={() => {
+                    <button className="b7" onClick={() => {
                         
                             setShow(show +7)
                     }}>7</button>
-                    <button onClick={() => {
+                    <button className="b6" onClick={() => {
                         
                             setShow(show +6)
                     }}>6</button>
-                    <button onClick={() => {
+                    <button className="b5" onClick={() => {
                         
                             setShow(show +5)
                     }}>5</button>
-                    <button onClick={() => {
+                    <button className="b4" onClick={() => {
                         
                         
                             setShow(show +4) 
                         
                     }}>4</button>
-                    <button onClick={() => {
+                    <button className="b3" onClick={() => {
                         
                         
                             
@@ -107,17 +147,17 @@ export function App() {
                             setShow(show +3)
 
                     }}>3</button>
-                    <button onClick={() => {
+                    <button className="b2" onClick={() => {
                         
                         
                             setShow(show +2)
                         
                     }}>2</button>
-                    <button onClick={() => {
+                    <button className="b1" onClick={() => {
                         
                             setShow(show +1)
                     }}>1</button>
-                    <button onClick={() => {
+                    <button className="b0" onClick={() => {
                         
                             setShow(show +0)
                     }}>0</button>
@@ -145,7 +185,7 @@ export function App() {
                     <button onClick={() => {
                         setShow(show +"*")
                     }}>×</button>
-                    <button  onClick={(e) => {
+                    <button id="click" onClick={(e) => {
                         
                         try{
                             
